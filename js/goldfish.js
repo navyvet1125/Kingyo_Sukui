ac2ecee824d201a6fd6266f2bf286835693d3fe5
Evan Washington <enavy04@gmail.com>
2016-03-21 11:54:15 -0700

    Made goldfish object more modular.  Added timer based self determining logic to goldfish.  Goldfish object runs logic once created.

--- js/goldfish.js
+++ js/goldfish.js
@@ -1,51 +1,58 @@
-var Goldfish = function(canvas, x, y, theta){
+// Required arguments are canvas, x, y, angle, targetX, targetY, and speed.
+var Goldfish = function(canvas, x, y, theta, targetX, targetY, speed){
 	this.canvas = canvas;
 	this.x =x;
 	this.y =y;
 	this.length = 70;
 	this.width = 35;
-	this.targetX = 0;
-	this.targetY = 0;
-	this.speed = 5;
+	this.targetX = targetX;
+	this.targetY = targetY;
+	this.speed = speed;
 	this.theta = theta;
+	this.radian = theta*Math.PI/180;
 	this.isNormal = true;
-	this.isLively = (Math.floor(Math.random()*2)===0);
+	this.distanceToTarget =Math.atan2((this.x-this.targetX),(this.y-this.targetY));
+	this.init(); //Sarts the fish logic
+	// this.isLively = (Math.floor(Math.random()*2)===0);
 };
 
-Goldfish.prototype.display =function(obj){
-var x1 = obj.x;
-var y1 = obj.y;
-var direction = obj.theta;
-$canvas.drawEllipse({
-  fillStyle: '#fa6900',
-  strokeStyle: '#f38630',
-  strokeWidth: 1,
-  x: x1, y: y1,
-  width: 70, height: 35,
-  rotate:direction
-});
-
-
-
-
-var x2 = x1 + (Math.cos(direction*Math.PI/180))*50;
-var y2 = y1 + (Math.sin(direction*Math.PI/180))*50;
-
-// fish tail... fishtail.x = body.x + (cos(body.direction)*50); fishtail.y = body.y + (sin(body.direction)*50)
-$canvas.drawPolygon({
-  fillStyle: '#f38630',
-  strokeStyle:'#fa6900',
-  strokeWidth: 1,
-  x: x2, y: y2,
-  radius: 25,
-  sides: 3,
-  rotate: direction+30
-});
-
+//Display the goldfish in the canvas
+Goldfish.prototype.display =function(){
+	var obj = this;
+	var direction = obj.theta;
+	//Draws the body of the fish
+	$canvas.drawEllipse({
+	  fillStyle: '#fa6900',
+	  strokeStyle: '#f38630',
+	  strokeWidth: 1,
+	  x: obj.x, y: obj.y,
+	  width: 70, height: 35,
+	  rotate:direction
+	});
+	// Draw the tail of the fish
+	$canvas.drawPolygon({
+	  fillStyle: '#f38630',
+	  strokeStyle:'#fa6900',
+	  strokeWidth: 1,
+	  x: obj.x + (Math.cos(direction*Math.PI/180))*50,
+	  y: obj.y + (Math.sin(direction*Math.PI/180))*50,
+	  radius: 25,
+	  sides: 3,
+	  rotate: direction+30
+	});
 };
 
-Goldfish.prototype.action = function(obj){
-	console.log(obj);
+
+// Goldfish logic.  Makes each goldfish keep track of it's own individual logic
+Goldfish.prototype.init = function(){
+var workingTheta = this.theta;
+var obj = this;
+obj.timerId = window.setInterval(function(){
+	workingTheta=(workingTheta+1)%360;
+	obj.theta= (workingTheta-60)%360;
+	obj.x = obj.targetX + (Math.cos(workingTheta*Math.PI/180))*100;
+	obj.y = obj.targetY + (Math.sin(workingTheta*Math.PI/180))*100;
+},(100/this.speed));	
 };
 
 var $canvas = $('#myCanvas');
@@ -66,12 +73,11 @@ var b ={
 
 $canvas.drawArc(b);
 
-var fishes = [];
-for (var i =0; i<40; i++){
-	var x = Math.floor(Math.random()*650)+100;
-	var y = Math.floor(Math.random()*650)+100;
-	var theta = Math.floor(Math.random()*360);
-	fishes.push(new Goldfish($canvas, x, y, theta));
-}
+// Required arguments are canvas, x, y, angle, targetX, targetY, and speed.
 
-fishes.forEach(function(key){key.display(key);});
+var sawa = new Goldfish($canvas, 250,250, 0, 300,300,5 );
+
+var timer2Id = window.setInterval(function(){
+	$canvas.clearCanvas();
+	sawa.display();
+},42);
