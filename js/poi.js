daa8d867aa18a5065cb62c6c74705ca41e7fe011
Evan Washington <enavy04@gmail.com>
2016-03-21 18:26:49 -0700

    Created the poi object

---
+++ js/poi.js
@@ -0,0 +1,15 @@
+var Poi = function(){
+	this.x =0;
+	this.y = 0;
+	this.centerArray = [];
+	this.outerArray = [];
+	this.isCenter = true;
+	this.isOuter = true;
+	this.isUnderWater = false;
+	this.centerHealth = 40;
+	this.outerHealth = 80;
+};
+
+Poi.prototype.update = function(event){
+
+};
